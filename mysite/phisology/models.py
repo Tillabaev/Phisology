51d8981aa9b15ccff34cb33e258e8b0dc3bc224f
Adahan00-kg <20ikramidinovrinat07@gmail.com>
2024-12-19 21:38:23 +0600

    chages

--- mysite/phisology/models.py
+++ mysite/phisology/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class AboutMe(models.Model):
     bio = models.TextField()
     profession = models.CharField(max_length=35)
@@ -17,6 +18,7 @@ class MainWorld(models.Model):
     subscription = models.PositiveSmallIntegerField(help_text='количества людей на которые вы подписаны ')
 
 
+
 class Review(models.Model):
 
     feedback = models.CharField(max_length=35,help_text='отзывы моих клиентов')
@@ -25,3 +27,33 @@ class Review(models.Model):
 class Img(models.Model):
     img = models.ImageField(upload_to='review_img/')
     review_connect = models.ForeignKey(Review,related_name='photo',on_delete=models.CASCADE)
+
+
+
+
+class Consultation(models.Model):
+    title = models.CharField(max_length=512)
+    description = models.TextField(null=True, blank=True)
+    duration = models.CharField(max_length=512)
+    format = models.CharField(max_length=512)
+    feedback = models.CharField(max_length=512)
+    price = models.PositiveSmallIntegerField(default=100)
+
+    def __str__(self):
+        return f'{self.title} - {self.price}'
+
+class Consultation_Keys(models.Model):
+    consultation = models.ForeignKey(Consultation, related_name='con_keys', on_delete=models.CASCADE)
+    keys = models.CharField(max_length=512)
+
+
+
+class Registration(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32,null=True,blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    number = models.IntegerField()
+    country = models.CharField(max_length=32, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} - {self.email}'
